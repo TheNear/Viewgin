35dd10276e9c512b59641083d50cf06e4cc80a0a
TheNear <thenearx@gmail.com>
2021-02-02 12:24:11 +0300

    Init project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Wrap } from "@components/Wrap/Wrap";
-import Loader from "@img/loader.svg";
-import image from "@img/antd.png";
 import { MainWrapper } from "./AppStyle";
-
-// const Wrap = React.lazy(() => import("../Wrap/Wrap"));
 
 const App: React.FC = () => {
   return (
     <MainWrapper>
-      <Loader />
       <Wrap />
-      <img src={image} alt="some" />
     </MainWrapper>
   );
 };
